e23fb35c2da4397fe3cde6b272566734d8f50cd8
HaitemHK <haitemhekki@gmail.com>
2024-02-07 18:21:37 +0100

    session 5

---
+++ sess5/loops.js
@@ -0,0 +1,85 @@
+// let today = new Date().getDay();
+// switch (today){
+//     case 0:
+//         day = "Sunday"
+//         break;
+//     case 1:
+//         day = "Monday"
+//         break;
+//     case 2:
+//         day = "tuesday"
+//         break;
+//     case 3:
+//         day = "Wednesday"
+//         break;
+//     case 4:
+//         day = "Thursday"
+//         break;
+//     case 5:
+//         day = "Friday"
+//         break;
+//     case 6:
+//         day = "Saturday"
+//         break;
+// }
+// console.log(day)
+
+// let abbvr ;
+// abbvr= `DD`
+// switch(abbvr){
+//     case "DD":
+//         branche = "Devolopement digital"
+//     break;
+//     case "ID":
+//         branche = "Infrastructure digital"
+//     break;
+//     case "WD":
+//         branche = "Web Design"
+//     break;
+//     default:
+//         abbvr= "N/A"
+// }
+// document.getElementById(`paragraph`).textContent = branche;
+
+// let days = ["Monday","tuesday","wednesday","thursday","friday"]
+
+// for(i = 0; i<=4;i++){
+//     console.log(days[i]);
+// }
+
+// for(item in days){
+//     console.log(days[item]);
+// }
+
+// for (jr of days){
+//     console.log(jr);
+// } 
+
+
+// let text = ``
+// days.forEach(myfunc);
+// function myfunc(value, index, array){
+//     text += value +` `;
+//     }
+// console.log(text)
+
+// while loops
+
+let n = 0
+while(n<=10){
+    console.log(n)
+    n += 1
+
+}
+// do while loops
+let m = 10
+do{
+    console.log(m)
+    m -= 1
+}
+while(n >=0)
+
+for (let i=0;i<10;i++){
+    if(i ===3){continue}
+    texte += `the num is `+1+`<br>`  
+}
